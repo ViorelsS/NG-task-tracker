02062691c028f025f7361c47a980aa63927b6217
Viorel Strogoteanu <viorelss@icloud.com>
2024-10-28 11:34:38 +0100

    Blocked

--- README.md
+++ README.md
@@ -25,3 +25,7 @@ Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To u
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+
+#### Mi sono fermato al minuto 1:01.56 di 2:01:31
+
+Url: https://www.youtube.com/watch?v=3dHNOWTI7H8&t=3s
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,18 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, TasksComponent, FontAwesomeModule],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    TasksComponent,
+    FontAwesomeModule,
+    HttpClientModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
